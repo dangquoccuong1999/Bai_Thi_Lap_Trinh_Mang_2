3144d7b411fd7ba2903faf260fa1681b1c065478
DangCuong <cuongbattri1999@gmail.com>
2020-04-11 00:28:59 +0700

    a

---
+++ Model/DBConnect.php
@@ -0,0 +1,31 @@
+<?php
+
+
+class DBConnect
+{
+    public $user;
+    public $pass;
+    public $link;
+    public $options = array(
+        PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8",
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+    );
+
+    public function __construct()
+    {
+        $this->user = 'root';
+        $this->pass = '';
+        $this->link = "mysql:host=localhost;dbname=shop_dang_cuong_authentic";
+    }
+
+    public function connect()
+    {
+        try{
+            $conn = new PDO($this->link,$this->user,$this->pass,$this->options);
+
+        }catch (PDOException $e){
+            return $e->getMessage();
+        }
+        return $conn;
+    }
+}
